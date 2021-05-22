84916068d50c06799abaf9a09d99c3d04c1b77cc
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-05-22 16:53:01 -0400

    5-22-21 - Updated API url and installed new version of rails

---
+++ src/Components/component1.jsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+
+const Component1 = (props) => {
+
+    return (
+        <>
+            <h1>MAIN PAGE</h1>
+            <h2>{props.text}</h2>
+        </>
+
+    )
+}
+
+export default Component1
